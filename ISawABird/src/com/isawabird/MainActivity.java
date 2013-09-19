092a10fe05d134060c1e155e5620695d3079d684
Srihari Kulkarni <sriniketana@gmail.com>
2013-09-19 18:34:10 +0530

    Initial project

--- ISawABird/src/com/isawabird/MainActivity.java
+++ ISawABird/src/com/isawabird/MainActivity.java
@@ -1,7 +1,15 @@
 package com.isawabird;
 
+import java.util.Iterator;
+import java.util.List;
+
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.SaveCallback;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -10,6 +18,20 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		/* Initialize Parse */
+		ParseLogin.login(this);
+		
+		try{
+			List<ParseObject> lists = ParseUtils.getLists();
+			Iterator<ParseObject> i = lists.iterator();
+			while(i.hasNext()){
+				Log.d("ISawABird", i.next().getString("ListName"));
+			}
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}
+		
 	}
 
 	@Override
