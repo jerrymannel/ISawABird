cd52e503e30e58aff610257eba04fd26fd94d441
Pradeep S Bhat <prdpsbhat@gmail.com>
2013-11-16 16:41:34 +0530

    Moved heavy task to asynctask

--- ISawABird/src/com/isawabird/MainActivity.java
+++ ISawABird/src/com/isawabird/MainActivity.java
@@ -3,6 +3,7 @@ package com.isawabird;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -20,11 +21,9 @@ import com.isawabird.parse.extra.SyncUtils;
 import com.isawabird.test.DataLoader;
 import com.parse.Parse;
 import com.parse.ParseInstallation;
-import com.parse.ParseUser;
 
 public class MainActivity extends Activity {
 
-	static MainActivity act = null; 
 	TextView numberSpecies ; 
 	TextView currentListName ; 
 	Button mSawBirdButton;
@@ -43,23 +42,22 @@ public class MainActivity extends Activity {
 			} else {
 
 				setContentView(R.layout.activity_main);
-				
+				mSawBirdButton = (Button) findViewById(R.id.btn_isawabird);
+				numberSpecies = (TextView)findViewById(R.id.text_mode);
+				currentListName = (TextView)findViewById(R.id.text_location);
+
 				Parse.initialize(this, ParseConsts.APP_ID, ParseConsts.CLIENT_KEY);
-				
-				// TODO: move heavy work to asynctask
-				ParseUtils.updateCurrentLocation(); 
-				
+
+				// move heavy work to asynctask
+				new InitAsyncTask().execute();
+
+				ParseUtils.updateCurrentLocation();
+
 				/* Set up the sync service */
 				SyncUtils.createSyncAccount(this);
 				SyncUtils.triggerRefresh();
 				ParseInstallation.getCurrentInstallation().saveInBackground();
 
-				/* Initialize the checklists */
-				Log.i(Consts.TAG, "Starting checklist init...");
-				Utils.initializeChecklist(this, Utils.getChecklistName());
-				Log.i(Consts.TAG, "Checklist init complete");
-
-				mSawBirdButton = (Button) findViewById(R.id.btn_isawabird);
 				mSawBirdButton.setOnClickListener(new OnClickListener() {
 					@Override
 					public void onClick(View v) {
@@ -67,23 +65,11 @@ public class MainActivity extends Activity {
 						startActivity(loginIntent);					
 					}
 				});
+
 				// TODO: fix inconsistency in listID and listName
 				Log.i(Consts.TAG, "current List ID: " + Utils.getCurrentListID());
 				Log.i(Consts.TAG, "current List Name: " + Utils.getChecklistName());
 				Log.i(Consts.TAG, "current Username: " + ParseUtils.getCurrentUsername());
-				
-				numberSpecies = (TextView)findViewById(R.id.text_mode);
-				currentListName = (TextView)findViewById(R.id.text_location);
-
-				currentListName.setText(Utils.getCurrentListName());
-				DBHandler dh = DBHandler.getInstance(this);
-				// TODO: not happy with static access to Utils class in DBHandler
-				numberSpecies.setText(String.valueOf(dh.getBirdCountForCurrentList()));
-
-				// TODO: remove below line after dev
-				// put all the dump tables and testing in data loader
-				//new DataLoader(getApplicationContext()).load(this.getDatabasePath(DBConsts.DATABASE_NAME).getAbsolutePath());
-				new DataLoader(getApplicationContext()).srihariTestFunction(this.getDatabasePath(DBConsts.DATABASE_NAME).getAbsolutePath());
 			}
 		} catch(Exception ex) {
 			ex.printStackTrace();
@@ -128,11 +114,38 @@ public class MainActivity extends Activity {
 	private void logout() {
 		// TODO implement
 	}
-	public static Context getContext(){
-		return act;
-	}
 
 	/*public static ConnectivityManager getConnectivityManager(){
 		return (ConnectivityManager)act.getSystemService(CONNECTIVITY_SERVICE);
 	}*/
+
+
+	private class InitAsyncTask extends AsyncTask<Void, Void, Long> {
+
+		protected Long doInBackground(Void... params) {
+
+			/* Initialize the checklists */
+			Log.i(Consts.TAG, "Starting checklist init...");
+			try {
+				Utils.initializeChecklist(getApplicationContext(), Utils.getChecklistName());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			Log.i(Consts.TAG, "Checklist init complete");
+			
+			// TODO: remove below line after dev
+			// put all the dump tables and testing in data loader
+			//new DataLoader(getApplicationContext()).load(this.getDatabasePath(DBConsts.DATABASE_NAME).getAbsolutePath());
+			new DataLoader(getApplicationContext()).srihariTestFunction(getApplicationContext().getDatabasePath(DBConsts.DATABASE_NAME).getAbsolutePath());
+
+			DBHandler dh = DBHandler.getInstance(getApplicationContext());
+			// TODO: not happy with static access to Utils class in DBHandler
+			return dh.getBirdCountForCurrentList();
+		}
+
+		protected void onPostExecute(Long param) {
+			numberSpecies.setText(String.valueOf(param));
+			currentListName.setText(Utils.getCurrentListName());
+		}
+	}
 }
