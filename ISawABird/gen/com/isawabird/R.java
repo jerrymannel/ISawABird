67eb31f493a50b2a1dc184abf635846ab0ba3606
chethan <chethan@chethan-ThinkPad>
2013-11-25 22:15:05 +0530

    swipe to remove items from list

--- ISawABird/gen/com/isawabird/R.java
+++ ISawABird/gen/com/isawabird/R.java
@@ -18,6 +18,10 @@ public final class R {
         public static final int color_grey=0x7f070003;
         public static final int color_main_title=0x7f070001;
         public static final int color_white=0x7f070002;
+        public static final int fb_normal=0x7f070009;
+        public static final int fb_pressed=0x7f07000a;
+        public static final int gplus_normal=0x7f070007;
+        public static final int gplus_pressed=0x7f070008;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -36,45 +40,71 @@ public final class R {
         public static final int button_bottom_normal=0x7f020003;
         public static final int button_bottom_pressed=0x7f020004;
         public static final int button_bottom_selector=0x7f020005;
-        public static final int ic_launcher=0x7f020006;
-        public static final int main_button_isawabird=0x7f020007;
-        public static final int textarea_normal=0x7f020008;
+        public static final int button_fb_login_normal=0x7f020006;
+        public static final int button_fb_login_pressed=0x7f020007;
+        public static final int button_fb_login_selector=0x7f020008;
+        public static final int button_gplus_login_normal=0x7f020009;
+        public static final int button_gplus_login_pressed=0x7f02000a;
+        public static final int button_gplus_login_selector=0x7f02000b;
+        public static final int button_login_normal=0x7f02000c;
+        public static final int button_login_pressed=0x7f02000d;
+        public static final int button_login_selector=0x7f02000e;
+        public static final int fb=0x7f02000f;
+        public static final int gplus=0x7f020010;
+        public static final int ic_launcher=0x7f020011;
+        public static final int main_button_isawabird=0x7f020012;
+        public static final int separator=0x7f020013;
+        public static final int textarea_normal=0x7f020014;
     }
     public static final class id {
-        public static final int action_developerSettings=0x7f0a0019;
-        public static final int action_login=0x7f0a0017;
-        public static final int action_logout=0x7f0a0018;
-        public static final int action_settings=0x7f0a0016;
-        public static final int btn_isawabird=0x7f0a0000;
-        public static final int btn_login=0x7f0a000a;
-        public static final int btn_myLists=0x7f0a0004;
-        public static final int btn_settings=0x7f0a0005;
-        public static final int btn_signup=0x7f0a000c;
-        public static final int btn_skip=0x7f0a000b;
-        public static final int button_dev_showloginscreen=0x7f0a0015;
-        public static final int listTextView=0x7f0a0012;
-        public static final int listView=0x7f0a000e;
-        public static final int list_index=0x7f0a0010;
-        public static final int row_title=0x7f0a0011;
-        public static final int scrollView1=0x7f0a0013;
-        public static final int search_query=0x7f0a000d;
-        public static final int section_list_view=0x7f0a000f;
-        public static final int textView1=0x7f0a0014;
-        public static final int textView_currentList=0x7f0a0002;
-        public static final int textView_current_list_title=0x7f0a0003;
-        public static final int textView_title=0x7f0a0006;
-        public static final int text_confirm=0x7f0a0009;
-        public static final int text_email=0x7f0a0007;
-        public static final int text_mode=0x7f0a0001;
-        public static final int text_pass=0x7f0a0008;
+        public static final int action_login=0x7f0a0022;
+        public static final int action_logout=0x7f0a0023;
+        public static final int action_mylists=0x7f0a0020;
+        public static final int action_settings=0x7f0a0021;
+        public static final int btn_forgot_password=0x7f0a000a;
+        public static final int btn_isawabird=0x7f0a0004;
+        public static final int btn_login=0x7f0a0009;
+        public static final int btn_login_with_facebook=0x7f0a000d;
+        public static final int btn_login_with_google=0x7f0a000c;
+        public static final int btn_signup=0x7f0a000f;
+        public static final int btn_skip=0x7f0a000e;
+        public static final int button_dev_showloginscreen=0x7f0a001f;
+        public static final int listTextView=0x7f0a001d;
+        public static final int listView=0x7f0a0019;
+        public static final int list_index=0x7f0a001b;
+        public static final int mylistItem_close=0x7f0a0013;
+        public static final int mylistView=0x7f0a0011;
+        public static final int mylistsItem_name=0x7f0a0012;
+        public static final int mylists_title=0x7f0a0010;
+        public static final int mysightingsItem_close=0x7f0a0017;
+        public static final int mysightingsItem_name=0x7f0a0016;
+        public static final int mysightingsView=0x7f0a0015;
+        public static final int mysightings_title=0x7f0a0014;
+        public static final int row_title=0x7f0a001c;
+        public static final int scrollView1=0x7f0a001e;
+        public static final int search_query=0x7f0a0018;
+        public static final int section_list_view=0x7f0a001a;
+        public static final int textView_count_title=0x7f0a0002;
+        public static final int textView_currentList=0x7f0a0001;
+        public static final int textView_current_list_title=0x7f0a0000;
+        public static final int textView_or=0x7f0a000b;
+        public static final int textView_title=0x7f0a0005;
+        public static final int text_confirm=0x7f0a0008;
+        public static final int text_email=0x7f0a0006;
+        public static final int text_mode=0x7f0a0003;
+        public static final int text_pass=0x7f0a0007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int login=0x7f030001;
-        public static final int search=0x7f030002;
-        public static final int search_row=0x7f030003;
-        public static final int search_section_view=0x7f030004;
-        public static final int settings=0x7f030005;
+        public static final int mylists=0x7f030002;
+        public static final int mylists_row=0x7f030003;
+        public static final int mysightings=0x7f030004;
+        public static final int mysightings_row=0x7f030005;
+        public static final int search=0x7f030006;
+        public static final int search_row=0x7f030007;
+        public static final int search_section_view=0x7f030008;
+        public static final int settings=0x7f030009;
     }
     public static final class menu {
         public static final int main=0x7f090000;
@@ -82,23 +112,32 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
+        public static final int close=0x7f060018;
+        public static final int count=0x7f06001b;
         public static final int current_list=0x7f06000d;
         public static final int defaultText=0x7f06000e;
         public static final int description_short=0x7f060009;
+        public static final int dummy=0x7f060019;
         public static final int email=0x7f060007;
+        public static final int forgot_password=0x7f060014;
         public static final int hello_world=0x7f060002;
         public static final int loc_undef=0x7f060006;
         public static final int login=0x7f06000a;
-        public static final int logout=0x7f060012;
+        public static final int login_or=0x7f060017;
+        public static final int login_with_facebook=0x7f060016;
+        public static final int login_with_google=0x7f060015;
+        public static final int logout=0x7f060013;
         public static final int mode_offline=0x7f060005;
         public static final int mode_online=0x7f060004;
         public static final int my_list=0x7f06000f;
+        public static final int my_sightings=0x7f060010;
         public static final int password=0x7f060008;
-        public static final int search=0x7f060011;
-        public static final int settings=0x7f060010;
+        public static final int search=0x7f060012;
+        public static final int settings=0x7f060011;
         public static final int signup=0x7f06000c;
         public static final int skip=0x7f06000b;
         public static final int tag=0x7f060003;
+        public static final int zero=0x7f06001a;
     }
     public static final class style {
         /** 
--- ISawABird/src/com/isawabird/MySightingsActivity.java
+++ ISawABird/src/com/isawabird/MySightingsActivity.java
@@ -2,9 +2,6 @@ package com.isawabird;
 
 import java.util.ArrayList;
 
-import com.isawabird.db.DBHandler;
-import com.isawabird.parse.ParseUtils;
-
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
@@ -16,6 +13,10 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.isawabird.db.DBHandler;
+import com.isawabird.parse.ParseUtils;
+import com.isawabird.utilities.SwipeDismissListViewTouchListener;
+
 public class MySightingsActivity extends Activity {
 
 	private ArrayList<String> myList;
@@ -23,10 +24,10 @@ public class MySightingsActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.mysightings);
-		
+
 		Bundle b = getIntent().getExtras();
 		String listName = b.getString("listName");
-		
+
 		DBHandler mydbh = DBHandler.getInstance(MainActivity.getContext());
 		ArrayList<Sighting> myBirdLists = mydbh.getSightingsByListName(listName, ParseUtils.getCurrentUsername());
 
@@ -37,8 +38,26 @@ public class MySightingsActivity extends Activity {
 			Log.i(Consts.TAG, "Sighting :: " + sighting.getListName());
 			myList.add(sighting.getListName());
 		}
-		
-		listview.setAdapter(new MyListAdapter(this, myList));
+		final MyListAdapter listAdapter = new MyListAdapter(this, myList);
+		listview.setAdapter(listAdapter);
+
+		SwipeDismissListViewTouchListener touchListener = new SwipeDismissListViewTouchListener(listview,
+				new SwipeDismissListViewTouchListener.DismissCallbacks() {
+					@Override
+					public boolean canDismiss(int position) {
+						return true;
+					}
+
+					@Override
+					public void onDismiss(ListView listView, int[] reverseSortedPositions) {
+						for (int position : reverseSortedPositions) {
+							listAdapter.remove(listAdapter.getItem(position));
+						}
+						listAdapter.notifyDataSetChanged();
+					}
+				});
+		listview.setOnTouchListener(touchListener);
+		listview.setOnScrollListener(touchListener.makeScrollListener());
 	}
 
 	private class MyListAdapter extends ArrayAdapter<String> {
@@ -54,13 +73,13 @@ public class MySightingsActivity extends Activity {
 		public View getView(int position, View convertView, ViewGroup parent) {
 			LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 			View rowView = inflater.inflate(R.layout.mysightings_row, parent, false);
-			
+
 			TextView textView1 = (TextView) rowView.findViewById(R.id.mysightingsItem_name);
 			TextView textView2 = (TextView) rowView.findViewById(R.id.mysightingsItem_close);
-			
+
 			textView1.setText(values.get(position));
 			textView2.setText(" ");
-			
+
 			return rowView;
 		}
 	}
