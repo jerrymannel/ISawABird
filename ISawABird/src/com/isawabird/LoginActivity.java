f2afe6fcb669f86273099935f8ea3315f70e3338
Pradeep S Bhat <prdpsbhat@gmail.com>
2013-11-26 18:27:56 +0530

    Signup changes
    Please don't add anything under bin, gen directory

--- ISawABird/src/com/isawabird/LoginActivity.java
+++ ISawABird/src/com/isawabird/LoginActivity.java
@@ -25,49 +25,72 @@ import com.parse.SignUpCallback;
 
 public class LoginActivity extends Activity {
 
-	private Button mLoginButton;
 	private TextView tv_title;
-	private TextView mSignupButton;
-	private TextView mSkipButton;
 	private TextView tv_forgot;
 	private TextView tv_or;
+
+	private Button mLoginButton;
+	private TextView mSignupButton;
+	private TextView mSkipButton;
+
+	private TextView mShowSignupButton;
+	private TextView mShowLoginButton;
+
 	private EditText mUsernameText;
 	private EditText mPassText;
-	private Button twitterLogin ; 
-	
+	private EditText mPassConfirmText;
+
+	private Button mTwitterButton;
+	private Button mGoogleButton;
+	private Button mFacebookButton;
+
 	private static LoginActivity loginActivity ; 
-	
+
 	Typeface openSansLight;
 	Typeface openSansBold;
 	Typeface openSansBoldItalic;
 
+
+	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login);
 		loginActivity = this ;
-		
+
 		openSansLight = Typeface.createFromAsset(getAssets(), "fonts/OpenSans-Light.ttf");
 		openSansBold = Typeface.createFromAsset(getAssets(), "fonts/OpenSans-Bold.ttf");
 		openSansBoldItalic = Typeface.createFromAsset(getAssets(), "fonts/OpenSans-BoldItalic.ttf");
 
 		tv_title = (TextView)findViewById(R.id.textView_title);
+		tv_forgot = (TextView) findViewById(R.id.btn_forgot_password);
+		tv_or = (TextView) findViewById(R.id.textView_or);
+
 		mLoginButton = (Button) findViewById(R.id.btn_login);
 		mSignupButton = (TextView) findViewById(R.id.btn_signup);
 		mSkipButton = (TextView) findViewById(R.id.btn_skip);
-		tv_forgot = (TextView) findViewById(R.id.btn_forgot_password);
-		tv_or = (TextView) findViewById(R.id.textView_or);
+
+		mShowSignupButton = (TextView) findViewById(R.id.btn_showsignup);
+		mShowLoginButton = (TextView) findViewById(R.id.btn_showlogin);
+
 		mUsernameText = (EditText) findViewById(R.id.text_email);
 		mPassText = (EditText) findViewById(R.id.text_pass);
-		twitterLogin =(Button)findViewById(R.id.btn_login_with_twitter); 
-		
+		mPassConfirmText = (EditText) findViewById(R.id.text_confirm);
+
+		mTwitterButton = (Button) findViewById(R.id.btn_login_with_twitter);
+		mGoogleButton = (Button) findViewById(R.id.btn_login_with_google);
+		mFacebookButton = (Button) findViewById(R.id.btn_login_with_facebook);
+
 		tv_title.setTypeface(openSansBold);
-		mLoginButton.setTypeface(openSansBold);
-		mUsernameText.setTypeface(openSansLight);
-		mPassText.setTypeface(openSansLight);
 		tv_forgot.setTypeface(openSansLight);
 		tv_or.setTypeface(openSansBold);
+
+		mLoginButton.setTypeface(openSansBold);
 		mSignupButton.setTypeface(openSansBold);
 		mSkipButton.setTypeface(openSansBold);
+
+		mUsernameText.setTypeface(openSansLight);
+		mPassText.setTypeface(openSansLight);
+		mPassConfirmText.setTypeface(openSansLight);
 
 		mLoginButton.setOnClickListener(new OnClickListener() {
 			@Override
@@ -101,51 +124,56 @@ public class LoginActivity extends Activity {
 					Toast.makeText(getApplicationContext(), "Network not available", Toast.LENGTH_SHORT).show();
 					return;
 				}
-				
-				final String username = mUsernameText.getText().toString();
+
 				final String email = mUsernameText.getText().toString();
 				final String pass = mPassText.getText().toString();
-				
+				final String passConfirm = mPassConfirmText.getText().toString();
+
+				if(!pass.equals(passConfirm)) {
+					Toast.makeText(getApplicationContext(), "Passwords do not match", Toast.LENGTH_SHORT).show();
+					return;
+				}
+
 				ParseQuery<ParseUser> query = ParseUser.getQuery();
-				query.whereEqualTo("username", username);
+				query.whereEqualTo("username", email);
 				// TODO: add busy indicator
 				query.findInBackground(new FindCallback<ParseUser>() {
-				  public void done(List<ParseUser> objects, ParseException e) {
-				    if (e == null) {
-				        if(objects != null && objects.size() > 0) {
-				        	
-				        }
-				    } else {
-				        // Something went wrong.
-				    }
-				  }
-				});
+					public void done(List<ParseUser> objects, ParseException e) {
+						if (e == null) {
+							if(objects != null && objects.size() > 0) {
+								Toast.makeText(getApplicationContext(), "User with email '" + email + "' already exists", Toast.LENGTH_SHORT).show();
+								return;
+							} else {
+								ParseUser user = new ParseUser();
+								user.setUsername(email);
+								user.setPassword(pass);
+								user.setEmail(email);
 
-				// TODO: show busy indicator
-				ParseUser user = new ParseUser();
-				user.setUsername(email);
-				user.setPassword(pass);
-				user.setEmail(email);
-
-				user.signUpInBackground(new SignUpCallback() {
-					@Override
-					public void done(ParseException e) {
-						// TODO Auto-generated method stub
-						if (e != null) {
-							Toast.makeText(getApplicationContext(), "Not able to signup. Please try again later", Toast.LENGTH_SHORT).show();
-						} else {
-							Toast.makeText(getApplicationContext(), "Successfully signed up", Toast.LENGTH_SHORT).show();
-							ParseUser.logInInBackground(email, pass, new LogInCallback() {
-								@Override
-								public void done(ParseUser user,
-										ParseException e) {
-									if(user == null) {
-										Toast.makeText(getApplicationContext(), "Not able to login. Please try again later", Toast.LENGTH_SHORT).show();
-									} else {
-										showHome();
+								user.signUpInBackground(new SignUpCallback() {
+									@Override
+									public void done(ParseException e) {
+										// TODO Auto-generated method stub
+										if (e != null) {
+											Toast.makeText(getApplicationContext(), "Not able to signup. Please try again later", Toast.LENGTH_SHORT).show();
+										} else {
+											Toast.makeText(getApplicationContext(), "Successfully signed up", Toast.LENGTH_SHORT).show();
+											ParseUser.logInInBackground(email, pass, new LogInCallback() {
+												@Override
+												public void done(ParseUser user,
+														ParseException e) {
+													if(user == null) {
+														Toast.makeText(getApplicationContext(), "Not able to login. Please try again later", Toast.LENGTH_SHORT).show();
+													} else {
+														showHome();
+													}
+												}
+											});
+										}
 									}
-								}
-							});
+								});
+							}
+						} else {
+							Toast.makeText(getApplicationContext(), "Not able to signup. Please try again later", Toast.LENGTH_SHORT).show();
 						}
 					}
 				});
@@ -160,13 +188,35 @@ public class LoginActivity extends Activity {
 			}
 		});
 
-		twitterLogin.setOnClickListener(new OnClickListener() {
-			
+		mShowSignupButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				mShowSignupButton.setVisibility(View.GONE);
+				mLoginButton.setVisibility(View.GONE);
+				mShowLoginButton.setVisibility(View.VISIBLE);
+				mPassConfirmText.setVisibility(View.VISIBLE);
+				mSignupButton.setVisibility(View.VISIBLE);
+			}
+		});
+
+		mShowLoginButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				mShowLoginButton.setVisibility(View.GONE);
+				mSignupButton.setVisibility(View.GONE);
+				mPassConfirmText.setVisibility(View.GONE);
+				mShowSignupButton.setVisibility(View.VISIBLE);
+				mLoginButton.setVisibility(View.VISIBLE);
+			}
+		});
+
+		mTwitterButton.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
 				ParseTwitterUtils.initialize(ParseConsts.TWITTER_CONSUMER_KEY, ParseConsts.TWITTER_CONSUMER_SECRET); 
 				ParseTwitterUtils.logIn(LoginActivity.getLoginActivity(), new LogInCallback() {
-					
+
 					@Override
 					public void done(ParseUser user, ParseException ex) {
 						if (user == null){
@@ -179,15 +229,12 @@ public class LoginActivity extends Activity {
 				});
 			}
 		});
-		
-		//mEmailText.setText("sriniketana");
-		//mPassText.setText("test123");
 	}
 
 	public static LoginActivity getLoginActivity(){
 		return loginActivity;		
 	}	
-	
+
 	private void showHome() {
 		Log.i(Consts.TAG, "TO HOME");
 		Utils.setFirstTime(false);
@@ -197,7 +244,4 @@ public class LoginActivity extends Activity {
 		startActivity(homeIntent);
 		finish();
 	}
-	
-	
-
 }
